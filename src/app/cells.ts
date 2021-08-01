c983cf15c6cc94bfd2913c5eddc7075a9a6480a6
Rene Saarsoo <nene@triin.net>
2021-08-01 15:54:39 +0300

    Build function, then evaluate it

--- src/app/cells.ts
+++ src/app/cells.ts
@@ -1,5 +1,13 @@
+type FormulaFn = (...args: number[]) => number;
+
 export type NumberCell = {type: "number"; value: number};
-export type FormulaCell = {type: "formula"; formula: string; value: number};
+export type FormulaCell = {
+  type: "formula";
+  formula: string;
+  value: number;
+  fn: FormulaFn;
+  params: string[];
+};
 export type EmptyCell = {type: "empty"};
 export type ErrorCell = {type: "error", value: string};
 
@@ -9,7 +17,13 @@ export type CellMap = Map<string, Cell>;
 export type CellType = "number" | "error" | "formula" | "empty";
 
 export const mkNumber = (value: number): NumberCell => ({type: "number", value});
-export const mkFormula = (formula: string, value: number): FormulaCell => ({type: "formula", formula, value});
+export const mkFormula = (formula: string, fn: FormulaFn, params: string[], value: number): FormulaCell => ({
+  type: "formula",
+  formula,
+  fn,
+  params,
+  value,
+});
 export const mkError = (value: string): ErrorCell => ({type: "error", value});
 export const mkEmpty = (): EmptyCell => ({type: "empty"});
 
@@ -23,7 +37,8 @@ export const mkCell = (s: string): Cell => {
   if (/^=.*$/.test(s)) {
     const formula = s.slice(1);
     try {
-      return mkFormula(formula, evaluate(formula));
+      const [fn, params] = mkFunc(formula);
+      return mkFormula(formula, fn, params, evaluate(fn));
     } catch (e) {
       return mkError(s);
     }
@@ -35,11 +50,21 @@ export const getCell = (name: string, matrix: CellMap): Cell => {
   return matrix.get(name) || mkEmpty();
 }
 
-const evaluate = (formula: string): number => {
-  const n = eval(formula);
+const evaluate = (fn: FormulaFn): number => {
+  const n = fn();
   if (typeof n === "number") {
     return n;
   } else {
     throw new Error("Formula doesn't evaluate to number");
   }
+}
+
+const mkFunc = (formula: string): [FormulaFn, string[]] => {
+  const params = extractParams(formula);
+  const fn = new Function(...params, `return ${formula};`) as FormulaFn;
+  return [fn, params];
+};
+
+const extractParams = (formula: string): string[] => {
+  return formula.match(/[A-Z][0-9]+/g) || [];
 }

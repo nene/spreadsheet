8039c6abe4a6307a8bd2652201dd3ab39987f106
Rene Saarsoo <nene@triin.net>
2021-08-07 18:11:41 +0300

    Move focus handing out of Editor to CellView

--- src/sheet/CellView.tsx
+++ src/sheet/CellView.tsx
@@ -1,6 +1,6 @@
 import { useDispatch } from "react-redux";
 import styled from "styled-components";
-import { CellCoord } from "../app/cells/cells";
+import { Cell, CellCoord, CellType } from "../app/cells/cells";
 import { selectCell, setCellValue } from "../app/cells/cellsSlice";
 import { focusCell, selectFocusedCoord } from "../app/focus";
 import { useAppSelector } from "../app/hooks";
@@ -16,20 +16,72 @@ export const CellView = ({coord}: CellViewProps) => {
   const dispatch = useDispatch();
 
   return (
-    <TableCell>
-      <Editor
-        coord={coord}
-        value={cell}
-        focused={coord === focusedCoord}
-        onChange={(value) => dispatch(setCellValue({coord, value}))}
-        onFocus={() => dispatch(focusCell(coord))}
-      />
+    <TableCell onClick={() => dispatch(focusCell(coord))}>
+      {coord === focusedCoord
+        ? <Editor
+          coord={coord}
+          cell={cell}
+          onChange={(value) => dispatch(setCellValue({coord, value}))}
+        />
+        : <ValueView cell={cell} />}
     </TableCell>
   );
 };
 
 const TableCell = styled.td`
-  position: relative;
   border: none;
   padding: 0;
 `;
+
+const ValueView = ({cell}: {cell: Cell}) => (
+  <ValueEl cellType={cell.type}>
+    {cellLabel(cell) ? <Label>{cellLabel(cell)}</Label> : undefined}
+    {cellValue(cell)}
+  </ValueEl>
+);
+
+const ValueEl = styled.div<{cellType: CellType}>`
+  position: relative;
+  display: block;
+  width: 70px;
+  height: 20px;
+  border-style: solid;
+  border-width: 1px;
+  border-color: ${({cellType}) => cellColor(cellType)};
+  margin: 0;
+  padding: 1px 2px;
+`;
+
+const Label = styled.span`
+  position: absolute;
+  top: 0;
+  right: 0;
+  color: #fff;
+  background-color: #2c8b7c;
+  font-size: 10px;
+  padding: 0 2px;
+  height: 11px;
+  line-height: 11px;
+`;
+
+const cellLabel = (cell: Cell): string | undefined => {
+  return cell.type === "formula" ? cell.name : undefined;
+};
+
+const cellColor = (type: CellType): string => {
+  switch (type) {
+    case "empty": return "#ccc";
+    case "number": return "#ccc";
+    case "formula": return "#7ca2ce";
+    case "error": return "red";
+  }
+}
+
+const cellValue = (cell: Cell): string => {
+  switch (cell.type) {
+    case "empty": return "";
+    case "number": return String(cell.value);
+    case "formula": return String(cell.value ?? "#UNEVAL");
+    case "error": return cell.value;
+  }
+}
--- src/sheet/Editor.tsx
+++ src/sheet/Editor.tsx
@@ -1,76 +1,38 @@
 import styled from "styled-components";
-import { CellType, Cell, CellCoord } from "../app/cells/cells";
+import { Cell, CellCoord } from "../app/cells/cells";
 
 interface EditorProps {
   coord: CellCoord;
-  value: Cell;
-  focused: boolean;
+  cell: Cell;
   onChange: (value: string) => void;
-  onFocus: () => void;
 }
 
-export const Editor = ({coord, value, focused, onChange, onFocus}: EditorProps) => {
+export const Editor = ({cell, onChange}: EditorProps) => {
   return (
-    <>
-      {cellLabel(value) && !focused ? <Label>{cellLabel(value)}</Label> : undefined}
-      <EditorEl
-        value={cellValue(value, focused)}
-        cellType={value.type}
-        onChange={(e) => onChange(e.target.value)}
-        onFocus={onFocus}
-      />
-    </>
+    <EditorEl
+      value={cellValue(cell)}
+      onChange={(e) => onChange(e.target.value)}
+      autoFocus={true}
+    />
   );
 };
 
-const Label = styled.span`
-  position: absolute;
-  top: 0;
-  right: 0;
-  color: #fff;
-  background-color: #2c8b7c;
-  font-size: 10px;
-  padding: 0 2px;
-  height: 11px;
-  line-height: 11px;
-`;
-
-const EditorEl = styled.input<{cellType: CellType}>`
+const EditorEl = styled.input`
   width: 70px;
   height: 20px;
   border-style: solid;
   border-width: 1px;
-  border-color: ${({cellType}) => cellColor(cellType)};
+  border-color: black;
   &:focus {
-    border: 1px solid black;
     outline: none;
   }
 `;
 
-const cellLabel = (cell: Cell): string | undefined => {
-  return cell.type === "formula" ? cell.name : undefined;
-};
-
-const cellColor = (type: CellType): string => {
-  switch (type) {
-    case "empty": return "default";
-    case "number": return "default";
-    case "formula": return "#7ca2ce";
-    case "error": return "red";
-  }
-}
-
-const cellValue = (cell: Cell, focused: boolean): string => {
+const cellValue = (cell: Cell): string => {
   switch (cell.type) {
     case "empty": return "";
     case "number": return String(cell.value);
-    case "formula": {
-      if (focused) {
-        return cell.formula;
-      } else {
-        return String(cell.value ?? "#UNEVAL");
-      }
-    }
+    case "formula": return cell.formula;
     case "error": return cell.value;
   }
 }

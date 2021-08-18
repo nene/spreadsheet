21521f6d499b3ca971e06b522ca855774e585606
Rene Saarsoo <nene@triin.net>
2021-08-18 15:34:02 +0300

    Extract createAreaMap() from focusAreas slice

--- src/app/areaMap.ts
+++ src/app/areaMap.ts
@@ -1,4 +1,5 @@
 import { CellCoord } from "./cells/cells";
+import { destructCellCoord, makeCellCoord } from "./cells/coord";
 
 export type CellSides = {
   top?: boolean;
@@ -8,3 +9,23 @@ export type CellSides = {
 };
 
 export type AreaMap = Record<CellCoord, CellSides>;
+
+export const createAreaMap = (coord1: CellCoord, coord2: CellCoord): AreaMap => {
+  const {x: x1, y: y1} = destructCellCoord(coord1);
+  const {x: x2, y: y2} = destructCellCoord(coord2);
+
+  const result: AreaMap = {};
+  for (let y = y1; y <= y2; y++) {
+    for (let x = x1; x <= x2; x++) {
+      if (x === x1 || x === x2 || y === y1 || y === y2) {
+        result[makeCellCoord({x, y})] = {
+          left: x === x1 || undefined,
+          right: x === x2 || undefined,
+          top: y === y1 || undefined,
+          bottom: y === y2 || undefined,
+        };
+      }
+    }
+  }
+  return result;
+};
--- src/app/focusAreas.ts
+++ src/app/focusAreas.ts
@@ -1,7 +1,6 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { AreaMap, CellSides } from "./areaMap";
+import { AreaMap, CellSides, createAreaMap } from "./areaMap";
 import { CellCoord } from "./cells/cells";
-import { destructCellCoord, makeCellCoord } from "./cells/coord";
 import { RootState } from "./store";
 
 const focusAreasSlice = createSlice({
@@ -10,23 +9,7 @@ const focusAreasSlice = createSlice({
   reducers: {
     setFocusArea(state, action: PayloadAction<[CellCoord] | [CellCoord, CellCoord]>) {
       const [coord1, coord2] = action.payload;
-      const {x: x1, y: y1} = destructCellCoord(coord1);
-      const {x: x2, y: y2} = coord2 ? destructCellCoord(coord2) : {x: x1, y: y1};
-
-      const result: AreaMap = {};
-      for (let y = y1; y <= y2; y++) {
-        for (let x = x1; x <= x2; x++) {
-          if (x === x1 || x === x2 || y === y1 || y === y2) {
-            result[makeCellCoord({x, y})] = {
-              left: x === x1 || undefined,
-              right: x === x2 || undefined,
-              top: y === y1 || undefined,
-              bottom: y === y2 || undefined,
-            };
-          }
-        }
-      }
-      return result;
+      return createAreaMap(coord1, coord2 || coord1);
     },
   }
 });
